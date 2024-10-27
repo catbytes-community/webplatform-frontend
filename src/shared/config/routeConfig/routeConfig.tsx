0a171d724570a148dd7bb4d53a6a2da9c350454a
Anastasiia Kozlova <anautomnee@gmail.com>
2024-10-27 12:53:51 +0100

    Add MentorsPage to router

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -2,16 +2,19 @@ import { RouteProps } from "react-router-dom"
 import { NotFoundPage } from "../../../pages/NotFoundPage"
 import { MainPage } from "../../../pages/MainPage"
 import { AboutPage } from "../../../pages/AboutPage"
+import { MentorsPage } from "../../../pages/MentorsPage"
 
 export enum AppRoutes {
 	MAIN = "main",
 	ABOUT = "about",
+	MENTORS = "mentors",
 	NOT_FOUND = "not_found"
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
 	[AppRoutes.MAIN]: "/",
 	[AppRoutes.ABOUT]: "/about",
+	[AppRoutes.MENTORS]: "/mentors",
 	[AppRoutes.NOT_FOUND]: "*"
 }
 
@@ -23,6 +26,10 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
 	[AppRoutes.ABOUT]: {
 		path: RoutePath.about,
 		element: <AboutPage />
+	},
+	[AppRoutes.MENTORS]: {
+		path: RoutePath.mentors,
+		element:<MentorsPage/>
 	},
 	[AppRoutes.NOT_FOUND]: {
 		path: RoutePath.not_found,
