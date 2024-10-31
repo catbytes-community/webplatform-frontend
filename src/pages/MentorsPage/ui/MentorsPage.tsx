af24dcfe5c013d7df2f417bb3f617ba107de75ff
Anastasiia Kozlova <anautomnee@gmail.com>
2024-10-31 12:30:36 +0100

    Add mapping of testData

--- src/pages/MentorsPage/ui/MentorsPage.tsx
+++ src/pages/MentorsPage/ui/MentorsPage.tsx
@@ -2,14 +2,50 @@ import s from './MentorsPage.module.css';
 
 import { Card } from "../../../shared/ui";
 
+const mentorsData = [{
+    id: 1,
+    name: "Marcy",
+    lastName: "Kim",
+    direction: ['frontend', 'design'],
+    experience: 4
+}, {
+    id: 2,
+    name: "Minnie",
+    lastName: "Maus",
+    direction: ['backend'],
+    experience: 1
+}, {
+    id: 3,
+    name: "Hermione",
+    lastName: "Granger",
+    direction: ['test'],
+    experience: 6
+}];
+
+// const directionsCard: React.CSSProperties = {
+//     padding: "8px 4px",
+//     fontSize: "12px"
+// }
 
 export const MentorsPage = () => {
     return (
         <div className={s.mentorsContainer}>
-            <h1 className={s.mentorsTitle}>Mentors</h1>
-            <Card>
-                <h1>Mentor1</h1>
-            </Card>
+            <h2 className={s.mentorsTitle}>Mentors</h2>
+            {mentorsData?.map(mentor => (
+                <Card key={mentor?.id}>
+                    <p>Name: {mentor?.name}</p>
+                    <p>Last Name: {mentor?.lastName}</p>
+                    <div className={s.experienceContainer}>
+                        {mentor?.direction?.map((direction, ind) => (
+                            <Card  key={ind}>
+                                {direction}
+                            </Card>
+                        ))}
+                    </div>
+                    <p>Experience: {mentor?.experience} years</p>
+                </Card>
+            ))}
+            
         </div>
     );
 }
--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -4,9 +4,9 @@ type Props = {
     children: string | JSX.Element | JSX.Element[];
 }
 
-export const Card = ({children}: Props) => {
+export const Card = ({children}: Props, {style}: { style?: React.CSSProperties }) => {
     return (
-        <div className={s.cardContainer}>
+        <div className={s.cardContainer} style={style ? style : undefined}>
             {children}
         </div>
     )
