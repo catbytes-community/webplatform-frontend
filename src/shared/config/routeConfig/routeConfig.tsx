350a7a1c493d5e56bb1ef6e8c38d5584f5855445
mkim-vidatec <mkim@vidatec.com>
2024-11-02 09:59:19 +0000

    creates all projects page

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,31 +1,38 @@
-import { RouteProps } from "react-router-dom"
-import { NotFoundPage } from "../../../pages/NotFoundPage"
-import { MainPage } from "../../../pages/MainPage"
-import { AboutPage } from "../../../pages/AboutPage"
+import { RouteProps } from "react-router-dom";
+import { NotFoundPage } from "../../../pages/NotFoundPage";
+import { MainPage } from "../../../pages/MainPage";
+import { AboutPage } from "../../../pages/AboutPage";
+import { AllProjectsPage } from "../../../pages/AllProjectsPage";
 
 export enum AppRoutes {
-	MAIN = "main",
-	ABOUT = "about",
-	NOT_FOUND = "not_found"
+  MAIN = "main",
+  ABOUT = "about",
+  ALL_PROJECTS = "projects",
+  NOT_FOUND = "not_found",
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
-	[AppRoutes.MAIN]: "/",
-	[AppRoutes.ABOUT]: "/about",
-	[AppRoutes.NOT_FOUND]: "*"
-}
+  [AppRoutes.MAIN]: "/",
+  [AppRoutes.ABOUT]: "/about",
+  [AppRoutes.ALL_PROJECTS]: "/projects",
+  [AppRoutes.NOT_FOUND]: "*",
+};
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
-	[AppRoutes.MAIN]: {
-		path: RoutePath.main,
-		element: <MainPage />
-	},
-	[AppRoutes.ABOUT]: {
-		path: RoutePath.about,
-		element: <AboutPage />
-	},
-	[AppRoutes.NOT_FOUND]: {
-		path: RoutePath.not_found,
-		element: <NotFoundPage />
-	}
-}
+  [AppRoutes.MAIN]: {
+    path: RoutePath.main,
+    element: <MainPage />,
+  },
+  [AppRoutes.ABOUT]: {
+    path: RoutePath.about,
+    element: <AboutPage />,
+  },
+  [AppRoutes.ALL_PROJECTS]: {
+    path: RoutePath.projects,
+    element: <AllProjectsPage />,
+  },
+  [AppRoutes.NOT_FOUND]: {
+    path: RoutePath.not_found,
+    element: <NotFoundPage />,
+  },
+};
