8327c5bc36170f2ee44da00e096d93215905c84d
mkim-vidatec <mkim@vidatec.com>
2024-11-17 08:57:25 +0000

    changes login component

--- src/pages/LoginPage/ui/LoginPage.tsx
+++ src/pages/LoginPage/ui/LoginPage.tsx
@@ -1,5 +1,37 @@
 import style from "./LoginPage.module.css";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import {auth} from "../../../firebaseConfig.js";
 
 export function LoginPage() {
-  return <div className={style.main}>LoginPage</div>;
+  const email = "marina.kim@catbytes.io";
+  const password = "testpassword";
+  // post(/create-application) > name, last name, about me... >>> send as part of application
+  // when mentors review this application > Approve or Reject
+  // auth.currentUser > email, 
+
+  const registerUser = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      console.log("user", user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("errorCode", errorCode, errorMessage);
+    });
+  }
+ 
+  // return <div className={style.main}>LoginPage
+  //   <h1>LoginPage</h1>
+  //   <button onClick={registerUser}>Register User</button>
+  // </div>;
+
+  return (
+    <div className={style.main}>
+      <h1>LoginPage</h1>
+      <button onClick={registerUser}>Register User</button>
+    </div>
+  )
 }
