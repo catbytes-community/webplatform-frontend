229120402a43919e4ea4ab2cbb464bc68760262e
Anna <138930718+Kitsune1904@users.noreply.github.com>
2024-11-18 13:21:40 +0200

    add glogal font, add input file in form

--- src/pages/AllProjectsPage/ui/AllProjectsPage.tsx
+++ src/pages/AllProjectsPage/ui/AllProjectsPage.tsx
@@ -1,9 +1,15 @@
 import style from "./AllProjectsPage.module.css";
 import Button, {ButtonsEnum} from "../../../shared/ui/Button/Button.tsx";
+import {useState} from "react";
+import {ProjectForm} from "../ProlectForm/ui/ProjectForm.tsx";
 
 export const AllProjectsPage = () => {
-  return <div className={"holder"}>
+  const [isEdition, setEdition] = useState<boolean>(false);
+
+  return <div className={style.holder}>
     <Button label={"create project"} btnType={ButtonsEnum.PRIMARY} onClick={() => alert("Hello")}/>
     <p>All Projects Page</p>
+    <ProjectForm isEdition={isEdition}/>
+
   </div>;
 };
