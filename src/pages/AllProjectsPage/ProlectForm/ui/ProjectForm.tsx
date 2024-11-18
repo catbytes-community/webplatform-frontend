47219d75df87c8d7264deef7283d06501120827e
Anna <138930718+Kitsune1904@users.noreply.github.com>
2024-11-18 20:36:02 +0200

    add description area

--- src/pages/AllProjectsPage/ProlectForm/ui/ProjectForm.tsx
+++ src/pages/AllProjectsPage/ProlectForm/ui/ProjectForm.tsx
@@ -7,11 +7,6 @@ type ProjectFormProps = {
   isEdition: boolean
 }
 
-type FormDataType = {
-  projectName: string;
-  projectDuration: string;
-  projectPeriod: string
-}
 
 
 export const ProjectForm = (props: ProjectFormProps) => {
@@ -42,45 +37,67 @@ export const ProjectForm = (props: ProjectFormProps) => {
           <h1 className={style.title}>{props.isEdition ? 'Edit your' : 'Add new'} project</h1>
           <button className={ButtonsEnum.PRIMARY} type={"submit"}>{props.isEdition ? 'save changes' : 'create project'}</button>
         </div>
-        <div className={style.add_proj_holder}>
-          <div className={style.proj_img}>
-            <input type="file"
-                   name={'projectImage'}
-                   id="imageInput"
-                   accept="image/png,image/jpeg"
-                   onChange={handleImage}
-                   hidden
-            />
-            <label htmlFor="imageInput">
-              {previewSrc ? (
-                  <img
-                      src={previewSrc}
-                      alt="Uploaded image preview"
-                  />
-              ) : (
-                  <>
-                    image<CrossImg/>
-                  </>
-              )}
-            </label>
+        <div className={style.holder}>
+          <div className={style.main_part}>
+            <div className={style.proj_img}>
+              <input type="file"
+                     name={'projectImage'}
+                     id="imageInput"
+                     accept="image/png,image/jpeg"
+                     onChange={handleImage}
+                     hidden
+              />
+              <label htmlFor="imageInput">
+                {previewSrc ? (
+                    <img
+                        src={previewSrc}
+                        alt="Uploaded image preview"
+                    />
+                ) : (
+                    <>
+                      image<CrossImg/>
+                    </>
+                )}
+              </label>
+            </div>
+            <div className={style.proj_main}>
+              <div>
+                <label>Project name*</label>
+                <input name="projectName" required={true} placeholder={'Enter project name'}
+                       onChange={charactersCounter}
+                       maxLength={100}/>
+                <span>Characters left {counter}</span>
+              </div>
+              <div>
+                <label>Project duration*</label>
+                <input name="projectDuration" required={true} placeholder={'Ex: 1 month'} maxLength={25}/>
+                <span>How long will the project last?</span>
+              </div>
+              <div>
+                <label>Project period*</label>
+                <input type={'text'} name="projectPeriod" required={true} placeholder={'Ex: 1 Nov - 1 Dec'}
+                       maxLength={25}/>
+                <span>Estimate the scope of project </span>
+              </div>
+            </div>
           </div>
-          <div className={style.proj_main}>
+          <div className={style.description_part}>
             <div>
-              <label>Project name*</label>
-              <input name="projectName" required={true} placeholder={'Enter project name'} onChange={charactersCounter}
-                     maxLength={100}/>
-              <span>Characters left {counter}</span>
+              <h3>Add description</h3>
             </div>
-            <div>
-              <label>Project duration*</label>
-              <input name="projectDuration" required={true} placeholder={'Ex: 1 month'} maxLength={25}/>
-              <span>How long will the project last?</span>
-            </div>
-            <div>
-              <label>Project period*</label>
-              <input type={'text'} name="projectPeriod" required={true} placeholder={'Ex: 1 Nov - 1 Dec'}
-                     maxLength={25}/>
-              <span>Estimate the scope of project </span>
+            <div className={style.proj_description}>
+              <div >
+                <label>Project summary*</label>
+                <textarea name="projectSummary" required={true} placeholder={'Summarize the project in 1-2 sentences'}
+                       maxLength={200}/>
+                <span>Up to 200 characters</span>
+              </div>
+              <div>
+                <label>Detailed project description</label>
+                <textarea name="projectDetails" required={true} placeholder={'Describe the project’s goals, stages and expected outcomes'}
+                       maxLength={200}/>
+                <span>Up to 2 000 characters</span>
+              </div>
             </div>
           </div>
         </div>
