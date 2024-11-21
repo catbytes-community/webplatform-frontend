deb5e635f2531d9ae55a2e5f889653d09f123107
Anastasiia Kozlova <anautomnee@gmail.com>
2024-11-21 21:30:56 +0100

    Implement search, add shared component SearchInput

--- src/pages/AllProjectsPage/ui/AllProjectsPage.tsx
+++ src/pages/AllProjectsPage/ui/AllProjectsPage.tsx
@@ -1,31 +1,55 @@
 import s from "./AllProjectsPage.module.css"
 import {projectsData} from "../ProjectCard/api/dummyProjectsData.ts";
 import {ProjectCard} from "../ProjectCard/ui/ProjectCard.tsx";
-import {useMemo, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import ProjectsPagination from "../ProjectsPagination/ui/ProjectsPagination.tsx";
+import {SearchInput} from "../../../shared/ui";
+import {Project} from "../../../app/types/global";
 
 const PageSize = 5;
 
 export const AllProjectsPage = () => {
+    const [projects, setProjects] = useState<Project[]>([]);
     const [currentPage, setCurrentPage] = useState(1);
+    const [searchString, setSearchString] = useState("");
+
+    // TO DO - get from useSelector
+    const allProjects = projectsData;
+
+    useEffect(() => {
+        setProjects(allProjects);
+    }, [])
+
     const currentProjectsData = useMemo(() => {
         const firstPageIndex = (currentPage - 1) * PageSize;
         const lastPageIndex = firstPageIndex + PageSize;
-        return projectsData.slice(firstPageIndex, lastPageIndex);
-    }, [currentPage]);
+        return projects.slice(firstPageIndex, lastPageIndex);
+    }, [projects, currentPage]);
+
+    useEffect(() => {
+        if (searchString === "") {
+            setProjects(allProjects);
+        }
+
+        setProjects(allProjects.filter(project => project.title.toLowerCase().includes(searchString)));
+    }, [allProjects, searchString]);
+
+
     return <div className={s.container}>
-        <div className="flex">
+        <div className="flex align-middle justify-between">
             <h1 className={`font-montserrat text-black ${s.header}`} >Collaboration and projects</h1>
+            <SearchInput setValue={setSearchString}/>
         </div>
         <div className={s.divider}></div>
-        <div className="mb-8">
-            {currentProjectsData.map((project, index) => (<ProjectCard key={index} project={project} />))}
-        </div>
-        <ProjectsPagination
-            currentPage={currentPage}
-            totalCount={projectsData.length}
-            pageSize={PageSize}
-            onPageChange={page => setCurrentPage(page)}
-        />
+        {projects.length > 0 ? <><div className="mb-8">
+                {currentProjectsData.map((project, index) => (<ProjectCard key={index} project={project}/>))}
+            </div>
+            <ProjectsPagination
+                currentPage={currentPage}
+                totalCount={projectsData.length}
+                pageSize={PageSize}
+                onPageChange={page => setCurrentPage(page)}
+            /></> : <p className="mt-4">No projects found</p>}
+
     </div>;
 };
--- src/shared/ui/SearchInput/SearchInput.tsx
+++ src/shared/ui/SearchInput/SearchInput.tsx
@@ -1,5 +1,17 @@
-export const SearchInput = () => {
+import s from "./SearchInput.module.css";
+import MagnifyingGlassIcon from "../icons/MagnifyingGlassIcon.tsx";
+
+type SearchInputProps = {
+    setValue: (value: string) => void;
+}
+
+export const SearchInput = ({setValue}:SearchInputProps) => {
     return (
-        <></>
+        <div className="relative">
+            <input type='text' placeholder='Search' className={s.input} onChange={(e) => setValue(e.target.value)} />
+            <div className="absolute top-6 right-2">
+                <MagnifyingGlassIcon/>
+            </div>
+        </div>
     );
 }
