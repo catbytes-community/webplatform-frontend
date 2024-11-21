6e9602c418be103879ff7c8bc01797c7bbe9366d
Anastasiia Kozlova <anautomnee@gmail.com>
2024-11-21 14:00:57 +0100

    Restructure Projects Routes and Components to divide All Projects from Add Project

--- src/pages/AllProjectsPage/ui/AllProjectsPage.tsx
+++ src/pages/AllProjectsPage/ui/AllProjectsPage.tsx
@@ -1,19 +1,5 @@
-import style from "./AllProjectsPage.module.css";
-import Button, {ButtonsEnum} from "../../../shared/ui/Button/Button.tsx";
-import React, {useState} from "react";
-import {ProjectForm} from "../ProlectForm/ui/ProjectForm.tsx";
-import Header from "../../../shared/ui/Header/Header.tsx";
+//import s from "./AllProjectsPage.module.css"
 
 export const AllProjectsPage = () => {
-  const [isEdition, setEdition] = useState<boolean>(false);
-
-  return <div>
-    <div>
-      <Header text={'Here must be menu'}/>
-    </div>
-    <div className={style.holder}>
-      <ProjectForm isEdition={isEdition}/>
-    </div>
-
-  </div>;
+    return <></>;
 };
