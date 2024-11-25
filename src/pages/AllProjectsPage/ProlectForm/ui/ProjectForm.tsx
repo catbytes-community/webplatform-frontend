006a4109d0c974938fbfd93550cb43328ce620b9
mkim-vidatec <mkim@vidatec.com>
2024-11-25 14:00:25 +0000

    resolves conflicts

--- src/pages/AllProjectsPage/ProlectForm/ui/ProjectForm.tsx
+++ src/pages/AllProjectsPage/ProlectForm/ui/ProjectForm.tsx
@@ -1,13 +1,22 @@
 import style from "./ProjectForm.module.css";
-import React, {useCallback, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import CrossImg from './cross.svg?react'
+import Button, {ButtonsEnum} from "../../../../shared/ui/Button/Button.tsx";
 
 type ProjectFormProps = {
   isEdition: boolean
 }
 
+type FormDataType = {
+  projectName: string;
+  projectDuration: string;
+  projectPeriod: string
+}
+
+
 export const ProjectForm = (props: ProjectFormProps) => {
   const [previewSrc, setPreviewSrc] = useState<string | null>(null);
+  const [counter, setCounter] = useState<number>(100);
 
   const handleImage = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     const file: File = event.target.files?.[0];
@@ -20,29 +29,60 @@ export const ProjectForm = (props: ProjectFormProps) => {
     }
   },[]);
 
+  const charactersCounter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const text: string = event.target.value;
+    setCounter(100 - text.length);
+  }
+
+//TODO: add action to form and onSubmit
   return (
-    <div className={style.add_proj_holder}>
-      <h1 className={style.title}>{props.isEdition ? 'Edit your' : 'Add new'} project</h1>
-      <form>
-        <div className={style.proj_img}>
-          <input type="file"
-                 id="imageInput"
-                 accept="image/png,image/jpeg"
-                 onChange={handleImage}
-                 hidden
-          />
-          <label htmlFor="imageInput" >
-            {previewSrc ? (
-                <img
-                    src={previewSrc}
-                    alt="Uploaded image preview"
-                />
-            ) : (
-                <>
-                  image<CrossImg/>
-                </>
-            )}
-          </label>
+    <div>
+      <form method={'post'} action={''}>
+        <div className={style.proj_header}>
+          <h1 className={style.title}>{props.isEdition ? 'Edit your' : 'Add new'} project</h1>
+          <button className={ButtonsEnum.PRIMARY} type={"submit"}>{props.isEdition ? 'save changes' : 'create project'}</button>
+        </div>
+        <div className={style.add_proj_holder}>
+          <div className={style.proj_img}>
+            <input type="file"
+                   name={'projectImage'}
+                   id="imageInput"
+                   accept="image/png,image/jpeg"
+                   onChange={handleImage}
+                   hidden
+            />
+            <label htmlFor="imageInput">
+              {previewSrc ? (
+                  <img
+                      src={previewSrc}
+                      alt="Uploaded image preview"
+                  />
+              ) : (
+                  <>
+                    image<CrossImg/>
+                  </>
+              )}
+            </label>
+          </div>
+          <div className={style.proj_main}>
+            <div>
+              <label>Project name*</label>
+              <input name="projectName" required={true} placeholder={'Enter project name'} onChange={charactersCounter}
+                     maxLength={100}/>
+              <span>Characters left {counter}</span>
+            </div>
+            <div>
+              <label>Project duration*</label>
+              <input name="projectDuration" required={true} placeholder={'Ex: 1 month'} maxLength={25}/>
+              <span>How long will the project last?</span>
+            </div>
+            <div>
+              <label>Project period*</label>
+              <input type={'text'} name="projectPeriod" required={true} placeholder={'Ex: 1 Nov - 1 Dec'}
+                     maxLength={25}/>
+              <span>Estimate the scope of project </span>
+            </div>
+          </div>
         </div>
       </form>
     </div>);
--- src/pages/AllProjectsPage/ui/AllProjectsPage.tsx
+++ src/pages/AllProjectsPage/ui/AllProjectsPage.tsx
@@ -1,15 +1,19 @@
 import style from "./AllProjectsPage.module.css";
 import Button, {ButtonsEnum} from "../../../shared/ui/Button/Button.tsx";
-import {useState} from "react";
+import React, {useState} from "react";
 import {ProjectForm} from "../ProlectForm/ui/ProjectForm.tsx";
+import Header from "../../../shared/ui/Header/Header.tsx";
 
 export const AllProjectsPage = () => {
   const [isEdition, setEdition] = useState<boolean>(false);
 
-  return <div className={style.holder}>
-    <Button label={"create project"} btnType={ButtonsEnum.PRIMARY} onClick={() => alert("Hello")}/>
-    <p>All Projects Page</p>
-    <ProjectForm isEdition={isEdition}/>
+  return <div>
+    <div>
+      <Header text={'Here must be menu'}/>
+    </div>
+    <div className={style.holder}>
+      <ProjectForm isEdition={isEdition}/>
+    </div>
 
   </div>;
 };
