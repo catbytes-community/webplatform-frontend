065d1fbcd4a37fee281509629805830d86763b07
Alina <ms.sciurus@gmail.com>
2024-12-10 11:58:38 +0200

    add post on server

--- src/pages/CreateApplicationPage/ui/CreateApplicationPage.tsx
+++ src/pages/CreateApplicationPage/ui/CreateApplicationPage.tsx
@@ -1,12 +1,6 @@
 import style from './CreateApplicationPage.module.css';
 import React, { useState } from 'react';
-// import { auth } from '/firebaseConfig';
-import firebaseConfig from '../../../../firebaseConfig';
-const { auth } = firebaseConfig;
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from 'firebase/auth';
+import axios from 'axios';
 import Button, { ButtonsEnum } from '../../../shared/ui/Button/Button';
 
 export const CreateApplicationPage: React.FC = () => {
@@ -15,129 +9,131 @@ export const CreateApplicationPage: React.FC = () => {
   const [about, setAbout] = useState<string>('');
   const [link, setLink] = useState<string>('');
   const [discord, setDiscord] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [isRegistering, setIsRegistering] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  //uncommit if need message in ui
+  // const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
-  const handleAuth = async (): Promise<void> => {
+  const handleSubmit = async (e: React.FormEvent): Promise<void> => {
+    e.preventDefault();
+
+    const data = {
+      name,
+      about,
+      email,
+      video_link: link,
+      discord_nickname: discord,
+    };
+
     try {
-      if (isRegistering) {
-        const userCredential = await createUserWithEmailAndPassword(
-          auth,
-          email,
-          password
-        );
-        alert(`User registered: ${userCredential.user.email}`);
-      } else {
-        const userCredential = await signInWithEmailAndPassword(
-          auth,
-          email,
-          password
-        );
-        alert(`User logged in: ${userCredential.user.email}`);
-      }
+      setError(null);
+      //uncommit if need message in ui
+      // setSuccessMessage(null);
+
+      // send data on server
+      const response = await axios.post(
+        'https://devapi.catbytes.io/applications',
+        data,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      // if response server sucsessful
+      alert('Application submitted successfully!');
+      console.log('Response:', response.data);
+
+      // clean form
+      setName('');
+      setEmail('');
+      setAbout('');
+      setLink('');
+      setDiscord('');
     } catch (error: any) {
-      alert(`Error: ${error.message}`);
+      // catch errors
+      if (axios.isAxiosError(error)) {
+        setError(
+          error.response?.data?.error ||
+            'An error occurred while submitting the form.'
+        );
+      } else {
+        setError('An unexpected error occurred.');
+      }
+
+      console.error('Error:', error);
     }
   };
 
   return (
     <div>
-      {/* <h1>{isRegistering ? 'Register' : 'Login'}</h1> */}
       <h1 className={style.title}>JOIN US</h1>
-      <div>
-        <input
-          className={style.input}
-          type="name"
-          placeholder="Name"
-          value={name}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setName(e.target.value)
-          }
+      {error && <p className={style.error}>{error}</p>}
+      {/* //uncommit if need message in ui 
+      {successMessage && <p className={style.success}>{successMessage}</p>}
+      */}
+      <form onSubmit={handleSubmit} className={style.form}>
+        <div>
+          <input
+            className={style.input}
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setName(e.target.value)
+            }
+          />
+        </div>
+        <div>
+          <input
+            className={style.input}
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setEmail(e.target.value)
+            }
+          />
+        </div>
+        <div>
+          <input
+            className={style.input}
+            type="text"
+            placeholder="About"
+            value={about}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setAbout(e.target.value)
+            }
+          />
+        </div>
+        <div>
+          <input
+            className={style.input}
+            type="text"
+            placeholder="Link to video"
+            value={link}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setLink(e.target.value)
+            }
+          />
+        </div>
+        <div>
+          <input
+            className={style.input}
+            type="text"
+            placeholder="Discord nickname"
+            value={discord}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setDiscord(e.target.value)
+            }
+          />
+        </div>
+        <Button
+          label="Submit"
+          btnType={ButtonsEnum.PRIMARY}
+          onClick={handleSubmit}
         />
-      </div>
-      <div>
-        <input
-          className={style.input}
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setEmail(e.target.value)
-          }
-        />
-      </div>
-      <div>
-        <input
-          className={style.input}
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setPassword(e.target.value)
-          }
-        />
-      </div>
-      <div>
-        <input
-          className={style.input}
-          type="text"
-          placeholder="About"
-          value={about}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setAbout(e.target.value)
-          }
-        />
-      </div>
-      <div>
-        <input
-          className={style.input}
-          type="text"
-          placeholder="Link to video"
-          value={link}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setLink(e.target.value)
-          }
-        />
-      </div>
-      <div>
-        <input
-          className={style.input}
-          type="text"
-          placeholder="Discord nickname"
-          value={discord}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setDiscord(e.target.value)
-          }
-        />
-      </div>
-      <button onClick={handleAuth}>
-        {isRegistering ? 'Register' : 'Login'}
-      </button>
-      <Button
-        label="Tertiary Button"
-        btnType={ButtonsEnum.TERTIARY}
-        onClick={() => setIsRegistering(!isRegistering)}
-      />
-      <Button
-        label="Register"
-        btnType={ButtonsEnum.SECONDARY}
-        onClick={() => setIsRegistering(!isRegistering)}
-      />
-      <Button
-        label="Log In"
-        btnType={ButtonsEnum.PRIMARY}
-        onClick={() => setIsRegistering(!isRegistering)}
-      />
-      <p>
-        {isRegistering ? 'Already have an account?' : 'Need to register?'}{' '}
-        <button onClick={() => setIsRegistering(!isRegistering)}>
-          {isRegistering ? 'Login' : 'Register'}
-        </button>
-      </p>
+      </form>
     </div>
   );
 };
-
-// export function CreateApplicationPage() {
-//   return <div className={style.main}>CreateApplicationPage</div>;
-// }
