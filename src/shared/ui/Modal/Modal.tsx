c50c092caf7534d6f2ba7e4658a2fe47e0a0782e
Alina Safina <safinalina1998@gmail.com>
2025-01-27 17:07:39 +0100

    style: fix modal display

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -13,7 +13,7 @@ export const Modal: React.FC<ModalProps> = (props) => {
     return(
         <>
         <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            className="justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
             <div className="relative w-auto my-6 mx-auto max-w-lg">
               {/*content*/}
