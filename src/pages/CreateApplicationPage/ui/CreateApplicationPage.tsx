9e10a54f8750d857b8e885af91ec24bb634040f0
mkim-vidatec <mkim@vidatec.com>
2025-02-02 14:03:19 +0000

    WIP: implements approve and reject apis

--- src/pages/CreateApplicationPage/ui/CreateApplicationPage.tsx
+++ src/pages/CreateApplicationPage/ui/CreateApplicationPage.tsx
@@ -1,61 +1,62 @@
-import style from './CreateApplicationPage.module.css';
-import React, { useState } from 'react';
-import axios from 'axios';
-import Button, { ButtonsEnum } from '../../../shared/ui/Button/Button';
+import style from "./CreateApplicationPage.module.css";
+import React, { useState } from "react";
+import axios from "axios";
+import Button, { ButtonsEnum } from "../../../shared/ui/Button/Button";
+import Navbar from "../../../shared/ui/Navbar/Navbar";
 
 export const CreateApplicationPage: React.FC = () => {
-  const [name, setName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [about, setAbout] = useState<string>('');
-  const [link, setLink] = useState<string>('');
-  const [discord, setDiscord] = useState<string>('');
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [about, setAbout] = useState<string>("");
+  const [link, setLink] = useState<string>("");
+  const [discord, setDiscord] = useState<string>("");
   const [agreeToTerms, setAgreeToTerms] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
   //uncommit if need message in ui
-  // const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
   // Validation rules
   const validateField = (field: string, value: string): string => {
     switch (field) {
-      case 'name':
+      case "name":
         return /^[A-Za-z\s]+$/.test(value)
-          ? ''
-          : 'Name must contain only letters and spaces.';
-      case 'email':
+          ? ""
+          : "Name must contain only letters and spaces.";
+      case "email":
         return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)
-          ? ''
+          ? ""
           : "Please enter a valid email address. Valid e-mail can contain only latin letters, numbers, '@' and '.'";
-      case 'about':
+      case "about":
         return value.trim().length >= 10
-          ? ''
-          : 'About must be at least 10 characters long.';
-      case 'link':
+          ? ""
+          : "About must be at least 10 characters long.";
+      case "link":
         return /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-]*)*$/.test(value)
-          ? ''
-          : 'Invalid video link. Valid link https://example.com';
-      case 'discord':
+          ? ""
+          : "Invalid video link. Valid link https://example.com";
+      case "discord":
         return /^(?=.{2,32}$)[a-zA-Z0-9._]+$/.test(value)
-          ? ''
-          : 'Discord username must be 2-32 characters long and can only contain letters, numbers, dots, and underscores.';
+          ? ""
+          : "Discord username must be 2-32 characters long and can only contain letters, numbers, dots, and underscores.";
       default:
-        return '';
+        return "";
     }
   };
 
   // Validate all fields before submission
   const validateForm = (): boolean => {
     const newErrors: { [key: string]: string } = {};
-    newErrors.name = validateField('name', name);
-    newErrors.email = validateField('email', email);
-    newErrors.about = validateField('about', about);
-    newErrors.link = validateField('link', link);
-    newErrors.discord = validateField('discord', discord);
+    newErrors.name = validateField("name", name);
+    newErrors.email = validateField("email", email);
+    newErrors.about = validateField("about", about);
+    newErrors.link = validateField("link", link);
+    newErrors.discord = validateField("discord", discord);
 
     setErrors(newErrors);
 
-    return Object.values(newErrors).every((error) => error === '');
+    return Object.values(newErrors).every((error) => error === "");
   };
 
   const handleBlur = (field: string, value: string): void => {
@@ -73,7 +74,7 @@ export const CreateApplicationPage: React.FC = () => {
     }
 
     if (!agreeToTerms) {
-      setError('You must agree to the Terms and Conditions.');
+      setError("You must agree to the Terms and Conditions.");
       return;
     }
 
@@ -81,37 +82,37 @@ export const CreateApplicationPage: React.FC = () => {
       name,
       about,
       email,
-      video_link: link,
-      discord_nickname: discord,
+      videoLink: link,
+      discordNickname: discord,
       agreeToTerms,
     };
 
     try {
       setError(null);
       //uncommit if need message in ui
-      // setSuccessMessage(null);
+      setSuccessMessage(null);
 
       // send data on server
 
       const response = await axios.post(
-        process.env.VITE_LINK_API_APPLICATIONS,
+        `${import.meta.env.VITE_DEVAPI}applications`,
         data,
         {
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
         }
       );
 
       // if response server sucsessful
-      alert('Application submitted successfully!');
+      alert("Application submitted successfully!");
 
       // clean form
-      setName('');
-      setEmail('');
-      setAbout('');
-      setLink('');
-      setDiscord('');
+      setName("");
+      setEmail("");
+      setAbout("");
+      setLink("");
+      setDiscord("");
       setAgreeToTerms(false);
       setErrors({});
     } catch (error: any) {
@@ -119,123 +120,127 @@ export const CreateApplicationPage: React.FC = () => {
       if (axios.isAxiosError(error)) {
         setError(
           error.response?.data?.error ||
-            'An error occurred while submitting the form.'
+            "An error occurred while submitting the form."
         );
       } else {
-        setError('An unexpected error occurred.');
+        setError("An unexpected error occurred.");
       }
 
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
   return (
     <div>
-      <h1 className={style.title}>JOIN US</h1>
-      {error && <p className={style.error}>{error}</p>}
-      {/* //uncommit if need message in ui 
-      {successMessage && <p className={style.success}>{successMessage}</p>}
-      */}
-      <form onSubmit={handleSubmit} className={style.form}>
-        <div>
-          <input
-            className={style.input}
-            type="text"
-            placeholder="Name"
-            value={name}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setName(e.target.value);
-              setErrors((prev) => ({
-                ...prev,
-                name: validateField('name', e.target.value),
-              }));
-            }}
+      <Navbar />
+      <div className="flex flex-col items-center justify-center gap-5">
+        <h1 className={style.title}>JOIN US</h1>
+        {error && <p className={style.error}>{error}</p>}
+
+        {successMessage && <p className={style.success}>{successMessage}</p>}
+
+        <form onSubmit={handleSubmit} className={style.form}>
+          <div>
+            <input
+              className={style.input}
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setName(e.target.value);
+                setErrors((prev) => ({
+                  ...prev,
+                  name: validateField("name", e.target.value),
+                }));
+              }}
+            />
+            {errors.name && <p className={style.error}>{errors.name}</p>}
+          </div>
+          <div>
+            <input
+              className={style.input}
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setEmail(e.target.value)
+              }
+              onBlur={(e: React.FocusEvent<HTMLInputElement>) =>
+                handleBlur("email", e.target.value)
+              }
+            />
+            {errors.email && <p className={style.error}>{errors.email}</p>}
+          </div>
+          <div>
+            <input
+              className={style.input}
+              type="text"
+              placeholder="About"
+              value={about}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setAbout(e.target.value);
+                setErrors((prev) => ({
+                  ...prev,
+                  about: validateField("about", e.target.value),
+                }));
+              }}
+            />
+            {errors.about && <p className={style.error}>{errors.about}</p>}
+          </div>
+          <div>
+            <input
+              className={style.input}
+              type="text"
+              placeholder="Link to video"
+              value={link}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setLink(e.target.value)
+              }
+              onBlur={(e: React.FocusEvent<HTMLInputElement>) =>
+                handleBlur("link", e.target.value)
+              }
+            />
+            {errors.link && <p className={style.error}>{errors.link}</p>}
+          </div>
+          <div>
+            <input
+              className={style.input}
+              type="text"
+              placeholder="Discord nickname"
+              value={discord}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setDiscord(e.target.value);
+                setErrors((prev) => ({
+                  ...prev,
+                  discord: validateField("discord", e.target.value),
+                }));
+              }}
+            />
+            {errors.discord && <p className={style.error}>{errors.discord}</p>}
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              checked={agreeToTerms}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setAgreeToTerms(e.target.checked)
+              }
+              className="mr-2"
+            />
+            <label>
+              I agree to the{" "}
+              <a href="/terms" target="_blank">
+                Terms and Conditions
+              </a>
+            </label>
+          </div>
+          <Button
+            label="Submit"
+            btnType={ButtonsEnum.PRIMARY}
+            onClick={handleSubmit}
           />
-          {errors.name && <p className={style.error}>{errors.name}</p>}
-        </div>
-        <div>
-          <input
-            className={style.input}
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setEmail(e.target.value)
-            }
-            onBlur={(e: React.FocusEvent<HTMLInputElement>) =>
-              handleBlur('email', e.target.value)
-            }
-          />
-          {errors.email && <p className={style.error}>{errors.email}</p>}
-        </div>
-        <div>
-          <input
-            className={style.input}
-            type="text"
-            placeholder="About"
-            value={about}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setAbout(e.target.value);
-              setErrors((prev) => ({
-                ...prev,
-                about: validateField('about', e.target.value),
-              }));
-            }}
-          />
-          {errors.about && <p className={style.error}>{errors.about}</p>}
-        </div>
-        <div>
-          <input
-            className={style.input}
-            type="text"
-            placeholder="Link to video"
-            value={link}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setLink(e.target.value)
-            }
-            onBlur={(e: React.FocusEvent<HTMLInputElement>) =>
-              handleBlur('link', e.target.value)
-            }
-          />
-          {errors.link && <p className={style.error}>{errors.link}</p>}
-        </div>
-        <div>
-          <input
-            className={style.input}
-            type="text"
-            placeholder="Discord nickname"
-            value={discord}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setDiscord(e.target.value);
-              setErrors((prev) => ({
-                ...prev,
-                discord: validateField('discord', e.target.value),
-              }));
-            }}
-          />
-          {errors.discord && <p className={style.error}>{errors.discord}</p>}
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            checked={agreeToTerms}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setAgreeToTerms(e.target.checked)
-            }
-          />
-          <label>
-            I agree to the{' '}
-            <a href="/terms" target="_blank">
-              Terms and Conditions
-            </a>
-          </label>
-        </div>
-        <Button
-          label="Submit"
-          btnType={ButtonsEnum.PRIMARY}
-          onClick={handleSubmit}
-        />
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
