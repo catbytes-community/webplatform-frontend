4b40e295a2b507050dfcdd0faf1525f1aeaf946d
mkim-vidatec <mkim@vidatec.com>
2025-02-08 16:09:32 +0000

    fixes pomodoro timer

--- src/pages/Pomodoro/ui/Pomodoro.tsx
+++ src/pages/Pomodoro/ui/Pomodoro.tsx
@@ -1,26 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 35ec07d (WIP)
 import { PomodoroPage } from "./PomodoroPage.tsx";
 import SettingsContextProvider from "../lib/SettingsContext.tsx";
 
 export const Pomodoro = () => {
-    return (
-        <SettingsContextProvider>
-            <PomodoroPage />
-        </SettingsContextProvider>
-    )
-}
-=======
-// import {PomodoroPage} from "./PomodoroPage.tsx";
-// import SettingsContextProvider from "../lib/SettingsContext.tsx";
-
-// export const Pomodoro = () => {
-//     return (
-//         <SettingsContextProvider>
-//             <PomodoroPage />
-//         </SettingsContextProvider>
-//     )
-// }
->>>>>>> origin/feat-13-applications-page
+  return (
+    <SettingsContextProvider>
+      <PomodoroPage />
+    </SettingsContextProvider>
+  );
+};
