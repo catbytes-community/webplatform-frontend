df1dfa587050560d465d607b154ce321249a18b2
mkim-vidatec <mkim@vidatec.com>
2025-03-02 14:12:21 +0000

    adds back dynamic base url

--- src/pages/LoginPage/ui/LoginPage.tsx
+++ src/pages/LoginPage/ui/LoginPage.tsx
@@ -73,13 +73,13 @@ export function LoginPage() {
 
     try {
       const actionCodeSettings = {
-        url: "http://localhost:5173/login",
-        // url:
-        //   import.meta.env.VITE_ENV === "localhost"
-        //     ? "http://localhost:5173/login"
-        //     : import.meta.env.VITE_ENV === "dev"
-        //     ? "https://dev.catbytes.io/login"
-        //     : "https://catbytes.io/login",
+        // url: "http://localhost:5173/login",
+        url:
+          import.meta.env.VITE_ENV === "localhost"
+            ? "http://localhost:5173/login"
+            : import.meta.env.VITE_ENV === "dev"
+            ? "https://dev.catbytes.io/login"
+            : "https://catbytes.io/login",
         handleCodeInApp: true,
       };
 
