08829e8278e6bb56581069d8f8f3e24a9e9b0de9
mkim-vidatec <mkim@vidatec.com>
2025-03-08 21:22:53 +0000

    removes unused footer

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,4 +1,3 @@
-import Footer from "../../../shared/ui/Footer/Footer";
 import Navbar from "../../../shared/ui/Navbar/Navbar";
 import AboutUsLP from "../../AboutUsSectionForLandingPage/AboutUsLP";
 import { HomeSection } from "./HomeSection";
