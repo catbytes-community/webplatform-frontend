c73122be5bcda81fa108ff45e2d361bdbc04509f
ElenaPavluchuk <elena.pvlch@gmail.com>
2025-07-28 21:54:14 +0300

    feat-102 mentor user profile page

--- src/pages/MentorUserProfilePage/ui/MentorUserProfilePage.tsx
+++ src/pages/MentorUserProfilePage/ui/MentorUserProfilePage.tsx
@@ -1,15 +1,49 @@
 import Navbar from "../../../shared/ui/Navbar/Navbar";
-//import { useEffect, useState } from "react";
-//import { useParams } from "react-router-dom";
-//import axios from "axios";
-//import { signOut } from "firebase/auth";
-//import { auth } from "../../../firebaseConfig";
-//import Cookies from "js-cookie";
-//import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+type Mentor = {
+  id: string;
+  name: string;
+  discord_nickname: string;
+  languages: string[];
+  //tags_name: string[]; // how to show mentor tags?
+  about: string; // it's experiance description?
+  // how to show mentor status ?
+};
 
 export default function MentorUserProfilePage() {
-  // const { id } = useParams();
-  // const navigate = useNavigate();
+  const { id } = useParams();
+  const [mentor, setMentor] = useState<Mentor | null>(null);
+
+  useEffect(() => {
+    const getMentor = async () => {
+      try {
+        // TODO: Fetch mentor from API
+        setMentor({
+          id: "1",
+          name: "John Doe",
+          discord_nickname: "johndoe#1234",
+          languages: ["JavaScript", "Python"],
+          //tags_name: ["Web Development", "AI"],
+          about: "5 years of experience in web development and AI.",
+        });
+      } catch (err) {
+        console.error("get mentor error", err);
+      }
+    };
+
+    try {
+      getMentor();
+    } catch (err) {
+      console.error("Catch error:", err);
+    }
+  }, [id]);
+
+  if (!mentor) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <Navbar />
@@ -19,20 +53,25 @@ export default function MentorUserProfilePage() {
         </h1>
         <p>
           <span className="font-bold font-montserrat">Name:</span>
+          {mentor?.name}
         </p>
         <p>
           <span className="font-bold font-montserrat">Discord Nickname:</span>
+          {mentor?.discord_nickname}
         </p>
         <p>
           <span className="font-bold font-montserrat">Languages:</span>
+          {mentor?.languages.join(", ")}
         </p>
-        <p>
+        {/* <p>
           <span className="font-bold font-montserrat">Tags:</span>
-        </p>
+          {mentor?.tags_name.join(", ")}
+        </p> */}
         <p>
           <span className="font-bold font-montserrat">
             Experience description:
           </span>
+          {mentor?.about}
         </p>
       </div>
     </div>
