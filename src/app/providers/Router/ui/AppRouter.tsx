d1899a0bf33ba945aff942e28b22f23629a27caa
Alina <ms.sciurus@gmail.com>
2025-10-03 14:54:47 +0300

    NEAT background added

--- src/app/providers/Router/ui/AppRouter.tsx
+++ src/app/providers/Router/ui/AppRouter.tsx
@@ -1,23 +1,21 @@
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { routeConfig } from "../../../../shared";
+import { routeConfig } from '../../../../shared';
 
 const AppRouter = () => (
-    <Routes>
-        {Object.values(routeConfig).map(({ element, path }) => (
-            <Route
-                key={path}
-                path={path}
-                element={(
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <div>
-                            {element}
-                        </div>
-                    </Suspense>
-                )}
-            />
-        ))}
-    </Routes>
+  <Routes>
+    {Object.values(routeConfig).map(({ element, path }) => (
+      <Route
+        key={path}
+        path={path}
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <div>{element}</div>
+          </Suspense>
+        }
+      />
+    ))}
+  </Routes>
 );
 
 export default AppRouter;
