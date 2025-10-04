7ecc409280b58cef1da5e048c71d23ecfd6f0952
Marina Kim <marina.kim.mk90@gmail.com>
2025-10-04 20:28:41 +0100

    feat-19-and-101: apply, approve and reject mentor appl

--- src/pages/ApplicationsPage/components/Application/MentorApplicationBlock.tsx
+++ src/pages/ApplicationsPage/components/Application/MentorApplicationBlock.tsx
@@ -3,9 +3,8 @@ import { MentorApplication } from "../../../../app/types/global";
 import Button, { ButtonsEnum } from "../../../../shared/ui/Button/Button";
 import style from "./ApplicationBlock.module.css";
 import ConfirmModal from "../../../../shared/ui/ConfirmModal/ConfirmModal";
-// import axios from "axios";
-// import { auth } from "../../../../firebaseConfig";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export const MentorApplicationBlock = ({
   application,
@@ -14,98 +13,51 @@ export const MentorApplicationBlock = ({
 }) => {
   const [isConfirmRejectShown, setIsConfirmRejectShown] = useState(false);
   const [isConfirmApproveShown, setIsConfirmApproveShown] = useState(false);
-  const [selectedApplication, setSelectedApplication] = useState<number | null>(null);
-//   const [comment, setComment] = useState("");
-
-//   const getProfileLink = async (userId: string) => {
-//     const response = await axios.get(
-//       `${import.meta.env.VITE_DEVAPI}users/${userId}`,
-//       {
-//         withCredentials: true,
-//       }
-//     );
-//     return response.data;
-//   };
-
-//   useEffect(() => {
-//     if (application.modified_by) {
-//       getProfileLink(application.modified_by).then((data) => {
-//         setMentorUser({
-//           id: data.id,
-//           name: data.name,
-//         });
-//       });
-//     }
-//   }, [application.modified_by]);
 
   const handleReject = async (confirm: boolean) => {
-    if(confirm) {
-      alert(`Mentor application ${selectedApplication} rejected`)
+    if (confirm) {
+      try {
+        await axios.patch(
+          `${import.meta.env.VITE_DEVAPI}mentors/${application.mentor_id}`,
+          {
+            status: "rejected",
+          },
+          {
+            withCredentials: true,
+          }
+        );
+
+        alert("Application rejected");
+      } catch (error) {
+        console.log(error);
+        alert("Error rejecting mentor application. Please try again later");
+      }
     }
-    
-    //   const user = localStorage.getItem("user")
-    //     ? JSON.parse(localStorage.getItem("user") as string)
-    //     : null;
-
-        // TODO: waiting for API to be ready
-    //   if (!comment || comment === "") {
-    //     alert("Please add a comment");
-    //     return;
-    //   } else {
-        // try {
-        //   await axios.put(
-        //     `${import.meta.env.VITE_DEVAPI}applications/${application.id}`,
-        //     {
-        //       status: "rejected",
-        //       comment: comment,
-        //       user_id: user.id,
-        //     },
-        //     {
-        //       withCredentials: true,
-        //     }
-        //   );
-
-        //   alert("Application rejected");
-        // //   setComment("");
-        // } catch (error) {
-        //   console.log(error);
-        // }
-    //   }
     setIsConfirmRejectShown(false);
-    setSelectedApplication(null);
   };
 
   const handleApprove = async (confirm: boolean) => {
-    if(confirm) {
-      alert(`Mentor application ${selectedApplication} approved`)
+    if (confirm) {
+      try {
+        const response = await axios.patch(
+          `${import.meta.env.VITE_DEVAPI}mentors/${application.mentor_id}`,
+          {
+            status: "active",
+          },
+          {
+            withCredentials: true,
+          }
+        );
+
+        if (response.status === 200) {
+          alert("Mentor application approved");
+        }
+      } catch (error) {
+        console.log(error);
+        alert("Error approving mentor application. Please try again later");
+      }
     }
-    
-    //   const user = localStorage.getItem("user")
-    //     ? JSON.parse(localStorage.getItem("user") as string)
-    //     : null;
-
-    // TODO: waiting for API to be ready
-    //   try {
-    //     const response = await axios.put(
-    //       `${import.meta.env.VITE_DEVAPI}mentors/${application.mentor_id}`,
-    //       {
-    //         status: "approved",
-    //         // user_id: user.id,
-    //       },
-    //       {
-    //         withCredentials: true,
-    //       }
-    //     );
-
-    //     if(response.status === 200) {
-    //         alert("Mentor application approved");
-    //     }
-    //   } catch (error) {
-    //     console.log(error);
-    //     alert("Error approving mentor application. Please try again later")
-    //   }
     setIsConfirmApproveShown(false);
-    setSelectedApplication(null);
   };
 
   return (
@@ -126,27 +78,16 @@ export const MentorApplicationBlock = ({
         data-id={application.mentor_id}
         className={`${style.application} w-[320px] sm:w-[500px]`}
       >
-        <p>Name: {application.name}</p>
+        <Link
+          to={`/user_profile/${application.user_id}`}
+          className="underline text-blue-500"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {application.name}
+        </Link>
         <p>About: {application.about}</p>
         <p>Mentor status: {application.status}</p>
-        {/* {application.status !== "pending" && (
-          <div>
-            <p>
-              Modified by:{" "}
-              <Link
-                to={`/user_profile/${mentorUser?.id}`}
-                className="underline text-blue-500"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {mentorUser?.name}
-              </Link>
-            </p>
-          </div>
-        )} */}
-        {/* {application.status === "rejected" && (
-          <p>Comment: {application.comment}</p>
-        )} */}
         {application.status === "pending" ? (
           <div className={style.buttons}>
             <Button
@@ -154,7 +95,6 @@ export const MentorApplicationBlock = ({
               btnType={ButtonsEnum.SECONDARY}
               onClick={() => {
                 setIsConfirmRejectShown(true);
-                setSelectedApplication(application.mentor_id)
               }}
             ></Button>
             <Button
@@ -162,13 +102,20 @@ export const MentorApplicationBlock = ({
               btnType={ButtonsEnum.PRIMARY}
               onClick={() => {
                 setIsConfirmApproveShown(true);
-                setSelectedApplication(application.mentor_id)
               }}
             ></Button>
           </div>
         ) : (
           <div className="flex justify-end">
-            <p className={`${application.status === 'rejected' ? 'bg-red-500' : application.status === 'inactive' ? 'bg-red-300' : 'bg-green-500'} p-1 rounded text-center font-bold text-white`}>
+            <p
+              className={`${
+                application.status === "rejected"
+                  ? "bg-red-500"
+                  : application.status === "inactive"
+                  ? "bg-red-300"
+                  : "bg-green-500"
+              } p-1 rounded text-center font-bold text-white`}
+            >
               {application.status.toUpperCase()}
             </p>
           </div>
--- src/pages/ApplicationsPage/ui/ApplicationsPage.tsx
+++ src/pages/ApplicationsPage/ui/ApplicationsPage.tsx
@@ -11,9 +11,11 @@ import { useNavigate } from "react-router-dom";
 
 export const ApplicationsPage = () => {
   const [applications, setApplications] = useState<Application[]>([]);
-  const [mentorApplications, setMentorApplications] = useState<MentorApplication[]>([]);
+  const [mentorApplications, setMentorApplications] = useState<
+    MentorApplication[]
+  >([]);
   const [filter, setFilter] = useState<string>("All");
-  const [filterType, setFilterType] = useState<string>("Members")
+  const [filterType, setFilterType] = useState<string>("Members");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -46,46 +48,25 @@ export const ApplicationsPage = () => {
             );
           }
           // get mentor applications
-          // TODO: API call, mock for now
-          setMentorApplications([
-            {
-              mentor_id: 1,
-              user_id: 1,
-              about: "Hi, my name is Marina and I would like to become a mentor",
-              status: "pending",
-              tags: ["react", "react-native", "nodejs"],
-              name: "Marina Kim"
-            },
-            {
-              mentor_id: 2,
-              user_id: 2,
-              about: "Hi, my name is Manu and I would like to become a mentor as well!",
-              status: "active",
-              tags: ["machine-learning", "python", "django"],
-              name: "Manu Serra"
-            },
-            {
-              mentor_id: 3,
-              user_id: 3,
-              about: "Hi, my name is User 3",
-              status: "rejected",
-              tags: ["game-dev", "java", "unix"],
-              name: "User Three"
-            },
-            {
-              mentor_id: 4,
-              user_id: 4,
-              about: "Hi, my name is User 4",
-              status: "inactive",
-              tags: ["nextjs", "javascript", "docker"],
-              name: "User Four"
-            },
-          ])
+          const getMentors = async () => {
+            const res = await axios.get(
+              `${import.meta.env.VITE_DEVAPI}mentors`,
+              {
+                withCredentials: true,
+              }
+            );
+            return res;
+          };
+
+          getMentors()
+            .then((res) => {
+              setMentorApplications(res?.data?.mentors);
+            })
+            .catch((err) => console.log("Error getting mentors", err));
         })
         .catch((error) => {
           console.error("Error fetching applications:", error);
           if (error.response?.status === 401) {
-
             signOut(auth)
               .then(() => {
                 axios.post(
@@ -117,15 +98,19 @@ export const ApplicationsPage = () => {
         className={`font-montserrat flex flex-col items-center ${style.container}`}
       >
         <h1 className="font-bold mb-5 text-xl mt-10">Applications</h1>
-        <div className='flex gap-2 mt-5 mb-5'>
-          <button 
-            className={`${style.filterButtons} ${filterType === "Members" ? "bg-black text-white" : ""}`}
+        <div className="flex gap-2 mt-5 mb-5">
+          <button
+            className={`${style.filterButtons} ${
+              filterType === "Members" ? "bg-black text-white" : ""
+            }`}
             onClick={() => setFilterType("Members")}
           >
             Members
           </button>
           <button
-            className={`${style.filterButtons} ${filterType === "Mentors" ? "bg-black text-white" : ""}`}
+            className={`${style.filterButtons} ${
+              filterType === "Mentors" ? "bg-black text-white" : ""
+            }`}
             onClick={() => setFilterType("Mentors")}
           >
             Mentors
@@ -133,42 +118,54 @@ export const ApplicationsPage = () => {
         </div>
         <div className="w-full flex justify-center gap-1 sm:gap-5 p-5">
           <button
-            className={`${style.filterButtons} ${filter === "All" ? "bg-black text-white" : ""
-              }`}
+            className={`${style.filterButtons} ${
+              filter === "All" ? "bg-black text-white" : ""
+            }`}
             onClick={() => setFilter("All")}
           >
             All
           </button>
           <button
-            className={`${style.filterButtons} ${filter === "Pending review" ? "bg-black text-white" : ""
-              }`}
+            className={`${style.filterButtons} ${
+              filter === "Pending review" ? "bg-black text-white" : ""
+            }`}
             onClick={() => setFilter("Pending review")}
           >
             Pending review
           </button>
           <button
-            className={`${style.filterButtons} ${filter === "Approved" ? "bg-black text-white" : ""
-              }`}
+            className={`${style.filterButtons} ${
+              filter === "Approved" ? "bg-black text-white" : ""
+            }`}
             onClick={() => setFilter("Approved")}
           >
             Approved
           </button>
           <button
-            className={`${style.filterButtons} ${filter === "Rejected" ? "bg-black text-white" : ""
-              }`}
+            className={`${style.filterButtons} ${
+              filter === "Rejected" ? "bg-black text-white" : ""
+            }`}
             onClick={() => setFilter("Rejected")}
           >
             Rejected
           </button>
         </div>
         <div className="overflow-y-auto max-h-[70vh] flex flex-col gap-5 p-5">
-          {filterType === "Members" && applications?.map((application) => (
-            <ApplicationBlock key={application.id} application={application} />
-          ))}
+          {filterType === "Members" &&
+            applications?.map((application) => (
+              <ApplicationBlock
+                key={application.id}
+                application={application}
+              />
+            ))}
 
-          {filterType === "Mentors" && mentorApplications?.map((application) => (
-            <MentorApplicationBlock key={application.mentor_id} application={application} />
-          ))}
+          {filterType === "Mentors" &&
+            mentorApplications?.map((application) => (
+              <MentorApplicationBlock
+                key={application.mentor_id}
+                application={application}
+              />
+            ))}
         </div>
       </div>
     </>
