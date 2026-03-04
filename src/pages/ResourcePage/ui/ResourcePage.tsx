b73e4d495c20b60cbce3f23a0c7b1e56e8ab67b3
ElenaPavluchuk <elena.pvlch@gmail.com>
2026-03-04 20:04:37 +0300

    feat-reso-171:add access check

--- src/pages/ResourcePage/ui/ResourcePage.tsx
+++ src/pages/ResourcePage/ui/ResourcePage.tsx
@@ -1,3 +1,76 @@
+import { useState, useEffect } from "react";
+import { useUser } from "../../../shared/lib/customHooks/useUser";
+import type { UserRole } from "../../../app/types/global";
+
+type Audience = "public" | "mentor" | "member";
+
+type Resource = {
+  id: string;
+  title: string;
+  type: string;
+  audience: Audience[];
+  description: string;
+  tags: string[] | null;
+};
+
 export default function ResourcePage() {
-  return <div>ResourcePage</div>;
+  const userIdFromLocalStorage = localStorage.getItem("userId")
+    ? Number(localStorage.getItem("userId"))
+    : null;
+  const { user } = useUser(userIdFromLocalStorage);
+  const [resource, setResource] = useState<Resource | null>(null);
+
+  useEffect(() => {
+    // TODO: fetch resource from API
+    const fetchedResource: Resource = {
+      id: crypto.randomUUID(),
+      title: "Test resource",
+      type: "post",
+      audience: ["mentor"],
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+      tags: null,
+    };
+
+    setResource(fetchedResource);
+  }, []);
+
+  const hasAccess = () => {
+    if (!resource) return false;
+
+    if (resource.audience.includes("public")) {
+      return true;
+    }
+
+    if (!user) {
+      return false;
+    }
+
+    const userRoles = user.roles.map((r) => r.role_name);
+    if (userRoles.includes("admin")) return true;
+
+    return resource.audience.some((requiredRole) => {
+      return userRoles.includes(requiredRole as UserRole);
+    });
+  };
+
+  if (!resource) {
+    return <div>Resource not found</div>;
+  }
+
+  if (!hasAccess()) {
+    return (
+      <div>
+        <p>You don't have access to view this page</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div>
+        <p>Resource Page</p>
+      </div>
+    </div>
+  );
 }
