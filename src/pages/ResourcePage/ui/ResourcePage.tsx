76d000a05b9e9944ebecdb2754b3a35d13bc69dc
ElenaPavluchuk <elena.pvlch@gmail.com>
2026-03-12 14:44:23 +0300

    feat-reso-171:add resource page design

--- src/pages/ResourcePage/ui/ResourcePage.tsx
+++ src/pages/ResourcePage/ui/ResourcePage.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import { useUser } from "../../../shared/lib/customHooks/useUser";
 import type { UserRole } from "../../../app/types/global";
+import style from "./ResourcePage.module.css";
+import Navbar from "../../../shared/ui/Navbar/Navbar";
+import Footer from "../../../shared/ui/Footer/Footer";
 
 type Audience = "public" | "mentor" | "member";
 
@@ -29,7 +32,7 @@ export default function ResourcePage() {
       audience: ["mentor"],
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-      tags: null,
+      tags: ["react", "javascript"],
     };
 
     setResource(fetchedResource);
@@ -55,7 +58,7 @@ export default function ResourcePage() {
   };
 
   if (!resource) {
-    return <div>Resource not found</div>;
+    return <div>Resource loading</div>;
   }
 
   if (!hasAccess()) {
@@ -67,10 +70,33 @@ export default function ResourcePage() {
   }
 
   return (
-    <div>
-      <div>
-        <p>Resource Page</p>
+    <div className="max-w-[1200px] px-5 sm:px-10 mx-auto">
+      <Navbar />
+      <div className="mt-10">
+        <h2 className="text-3xl md:text-4xl font-bold">{resource?.title}</h2>
       </div>
+
+      <div className="flex justify-between mt-5 md:mt-9 gap-5 flex-wrap md:flex-nowrap">
+        <div className="p-6 rounded-3xl w-full md:w-2/3 bg-white/40 shadow-[0_6px_10px_#ffa6ad66]">
+          <p className="text-lg md:text-xl font-montserrat">
+            {resource?.description}
+          </p>
+        </div>
+
+        <div
+          className={`${style.tagsContainer} justify-center md:justify-start w-full md:w-1/3 h-fit`}
+        >
+          {(resource.tags ?? []).map((tag) => (
+            <span
+              key={tag}
+              className={`${style.tags} text-sm md:text-base font-medium font-montserrat`}
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
