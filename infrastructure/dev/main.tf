b75b2608cfa06ecc0367d24f95b66b1f81041b9e
Marina Kim <marina.kim.mk90@gmail.com>
2026-04-19 08:42:50 +0100

    updates terraform script

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -79,6 +79,17 @@ resource "aws_cloudfront_distribution" "frontend" {
     ssl_support_method             = "sni-only"
     minimum_protocol_version       = "TLSv1.2_2021"
   }
+  custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
 }
 
 resource "aws_s3_bucket_policy" "frontend" {
